1316fcdfd78cbf7667ed5f33ed7edfc6c6aa40e9
l-yusya <105113818+l-yusya@users.noreply.github.com>
2022-10-14 15:01:49 +0300

    Code cleaned up

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
   const submit = document.getElementById("submit");
   let userEmail;
 
+  function openPopUp() {
+    document.getElementById("popUp").style.display = "block";
+    document.getElementById("confirmation__text").innerHTML = `Please confirm account creation for <em> ${userEmail} </em>`;
+  }
 
   document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
@@ -12,31 +16,25 @@
     openPopUp();
   });
 
-  
-  function openPopUp() {
-    document.getElementById("popUp").style.display = "block";
-    document.getElementById("confirmation__text").innerHTML = `Please confirm account creation for <em> ${userEmail} </em>`;
-  }
-
-
   const cancel = document.getElementById("cancel");
-
+  
   function closePopUp() {
     document.getElementById("popUp").style.display = "none";
   }
-
+  
   cancel.addEventListener("click", closePopUp);
+
 
   const confirm = document.getElementById("confirm");
   const status = document.getElementById("status__text");
-  // const welcomeMessage = document.getElementById("status__container");
   
   function handleSubmit() {
     closePopUp();
     form.style.display = "none";
     document.getElementById("status__container").style.display = "block";
-    status.innerHTML = `Hello, user with email <em>${userEmail}</em>!`;
+    status.innerHTML = `Hello, user with email <br> <em>${userEmail}</em>!`;
   }
 
   confirm.addEventListener("click", handleSubmit);
+
 })();
